f3ff93b514cd6dec34580f0afa2c2ae1ea00118c
Nicolai Rimmereid <nicolairimmereid@Nicolais-MacBook-Pro.local>
2020-12-01 00:45:21 +0100

    fix on findbyquizid

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,5 +1,5 @@
 const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions = () => questionsModel.find()
 const findQuestionById = (qid) => questionsModel.findById(qid)
-const findQuestionsForQuiz = (qzid) => questionsModel.findById(qzid)
+const findQuestionsForQuiz = (qid) => questionsModel.findOne({quizId: qid})
 module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
